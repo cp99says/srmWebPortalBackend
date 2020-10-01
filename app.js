6995d5674fcc63648dbe6ecd51968490db174240
chetan pareek <pareekchetan7891@gmail.com>
2020-10-02 00:20:41 +0530

    authorization and changes in project details done

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const verify=require('./routes/ktr_socp_get')
 const cors=require('cors')
 
 
-              mongoose.connect(`mongodb://localhost:27017/`,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{console.log(`db connected`)}).catch((err)=>{
-                 console.log(err)
-             })
-
-            //  mongoose.connect(`mongodb+srv://chetan_db:chetan_pwd@cluster0.gcabm.mongodb.net/srm?retryWrites=true&w=majority`,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{console.log(`db connected`)}).catch((err)=>{
+            //   mongoose.connect(`mongodb://localhost:27017/`,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{console.log(`db connected`)}).catch((err)=>{
             //      console.log(err)
             //  })
+
+              mongoose.connect(`mongodb+srv://chetan_db:chetan_pwd@cluster0.gcabm.mongodb.net/srm?retryWrites=true&w=majority`,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{console.log(`db connected`)}).catch((err)=>{
+                  console.log(err)
+              })
 
 app.use(express.json())
 app.use(cors())
